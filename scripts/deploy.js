5150cfc66bcbc757f2422c3b1e4e9180e9a03c42
abhay-8 <abhaynimmagadda@gmail.com>
2023-07-31 02:02:40 +0530

    Deployed scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,42 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const {ethers} =require("hardhat");
+
+require("dotenv").config({path:".env"});
+
+const {LINK_TOKEN, VRF_COORDINATOR, KEY_HASH, FEE} = require("../constants");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  /*
+ A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+ so randomWinnerGame here is a factory for instances of our RandomWinnerGame contract.
+ */
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+ const randomWinnerGame = await ethers.getContractFactory("RandomWinnerGame");
 
-  await lock.waitForDeployment();
+ const deployedRandomWinnerGameContract = await randomWinnerGame.deploy(VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+ await deployedRandomWinnerGameContract.deployed();
+
+ console.log("Verify Contract Address: ", deployedRandomWinnerGameContract.address);
+ console.log("sleeping..");
+ // Wait for etherscan to notice that the contract has been deployed
+ await sleep(30000);
+
+ // Verify the contract after deploying
+ await hre.run("verify:verify", {
+  address: deployedRandomWinnerGameContract.address,
+  constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+ });
+                                          
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
